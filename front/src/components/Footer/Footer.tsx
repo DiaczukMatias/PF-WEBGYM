bafdf05c231af1d48846ac1d0f1d56e09d57e562
Londarza <lautaro.ondarza@gmail.com>
2024-11-04 17:09:28 -0300

    se modifica footer: se agrega link contact, se cambian etiquetas a x Link

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaInstagram, FaLinkedin, FaPinterest, FaTiktok } from "react-icons/fa";
 
@@ -12,37 +13,38 @@ const Footer = () => {
 
         {/* Centro */}
         <div className="flex space-x-4 text-sm mb-4 md:mb-0">
-          <a href="/privacy" className="hover:text-secondary2">Privacy Policy</a>
-          <a href="/terms" className="hover:text-secondary2">Terms of Service</a>
+          <Link href="/privacy" className="hover:text-secondary2">Privacy Policy</Link>
+          <Link href="/terms" className="hover:text-secondary2">Terms of Service</Link>
+          <Link href="/contact" className="hover:text-secondary2">Contact</Link>
         </div>
 
         {/* derecha*/}
         <div className="flex space-x-4">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
             <span className="text-secondary hover:text-accent2 transition-colors duration-200">
               <FaFacebook size={20} />
             </span>
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
             <span className="text-secondary hover:text-accent2 transition-colors duration-200">
               <FaInstagram size={20} />
             </span>
-          </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
             <span className="text-secondary hover:text-accent2 transition-colors duration-200">
               <FaLinkedin size={20} />
             </span>
-          </a>
-          <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
             <span className="text-secondary hover:text-accent2 transition-colors duration-200">
               <FaPinterest size={20} />
             </span>
-          </a>
-          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
             <span className="text-secondary hover:text-accent2 transition-colors duration-200">
               <FaTiktok size={20} />
             </span>
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
