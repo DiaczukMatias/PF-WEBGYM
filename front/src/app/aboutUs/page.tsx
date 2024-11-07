fd644f2fc89ecbbb1f56389edc36d7f88144a60e
cami-lg96 <clg1196.henry@gmail.com>
2024-11-06 22:43:27 -0300

    concacto

--- front/src/app/aboutUs/page.tsx
+++ front/src/app/aboutUs/page.tsx
@@ -1,4 +1,4 @@
-/*import React from "react";
+import React from "react";
 import AboutUs from "@/views/AboutUs/AboutUs";
 
 const AboutUsPage = () => {
--- front/src/views/Contacto/Contacto.tsx
+++ front/src/views/Contacto/Contacto.tsx
@@ -2,9 +2,8 @@ import ContactForm from "@/components/Contact/Contact";
 
 const ContactoView = () => {
     return (
-      <div className="flex flex-wrap items-center gap-4 justify-center container mx-auto p-4">
-        <h1 className="text-2xl text-accent font-bold text-center m-4">Contacto</h1>
-        <p className="text-center text-sm font-extralight text-secondary m-8">Si tienes alguna pregunta, por favor completa el formulario a continuación y nos pondremos en contacto contigo pronto.</p>
+      <div>
+        <h1 className="flex justify-center font-bold font-sans text-xl text-accent m-4">CONTACTANOS:</h1>
         <ContactForm />
       </div>
       )
