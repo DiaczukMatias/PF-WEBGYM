a4f40408318f566750ae07648f5782243203e055
Londarza <lautaro.ondarza@gmail.com>
2024-11-12 11:30:49 -0300

    se modifica middleware para especificar rutas protegidas segun el rol del usuario,se crea ruta de admin para probar si funciona, por ahora queda vacia

---
+++ front/src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Admin = () => {
+    return (
+        <h1>Hola soy la  futura pagina de ADMINS</h1>
+    );
+  };
+  
+  export default Admin;
+  
--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -2,18 +2,33 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
+import { RolEnum } from "./interfaces/IUser";
 //no use withAuth ya que la redireccion por defecto es a otro link, no al nuestro
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
-
+  console.log(' TOKEN EN MIDDLEWARE'+token?.user?.rol);// ='cliente'
+  
   if (!token) {
 
     const loginUrl = new URL("/login", req.url);
     return NextResponse.redirect(loginUrl);
   }
 
+  const url = req.nextUrl.pathname;
+  const { rol } = token?.user;
 
+  if (url.startsWith('/admin')) {
+    if (rol !== RolEnum.ADMIN) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url)); // O la ruta de tu elección
+    }
+  }
+  if (url.startsWith('/profesores')) {
+    if (rol !== RolEnum.ADMIN && rol !== RolEnum.PROFESOR) {
+      return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+  }
+  
   return NextResponse.next();
 }
 
@@ -25,6 +40,8 @@ export const config = {
     "/agenda/:path*",
     "/subscription/:path*",
     "/clases/:path*",
+    "/admin/:path*",
+    "/profesores/:path*"
     // Agrega aquí más rutas que desees proteger
   ],
 };
