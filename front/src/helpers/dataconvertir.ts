4a92d2191bd3a91e4dd914669ddda167153b9379
cami-lg96 <161357194+cami-lg96@users.noreply.github.com>
2024-11-20 16:58:51 -0300

    Delete front/src/helpers directory

--- front/src/helpers/dataconvertir.ts
+++
@@ -1,7 +0,0 @@
-// helpers/dateHelpers.ts
-export const getDayOfWeek = (date: string) => {
-    const day = new Date(date).getDay();
-    const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-    return daysOfWeek[day];
-  };
-  
