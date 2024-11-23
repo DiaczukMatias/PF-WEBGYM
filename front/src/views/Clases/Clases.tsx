4e29af721b22f88289c65bfdeb857eaf6703dc9c
Londarza <lautaro.ondarza@gmail.com>
2024-11-23 18:04:58 -0300

    se crea ruta  para traer profesores, se modifican interfaces xq back envia nueva info , se cambia filtro de pag clases, funcionando ok como filtro combinado, todavia queda una falla en como manejar los errores para que se pueda buildear

--- front/src/views/Clases/Clases.tsx
+++ front/src/views/Clases/Clases.tsx
@@ -1,30 +1,32 @@
 "use client";
 import { useState, useEffect } from "react";
 import ClassCardList from "@/components/CardList/CardList";
-import { profesoresData } from "@/helpers/datatemporalClases"; // Simulación local de clases
 import { getCategories } from "@/helpers/Fetch/FetchCategorias"; // Fetch de categorías
 import { searchClases } from "@/helpers/Fetch/FetchClases"; // Buscar clases con parámetros
 import { fetchClases } from "@/helpers/Fetch/FetchClases"; // Obtener todas las clases
 import { ICategoria } from "@/interfaces/ICategory";
 import { ISearchResult } from "@/interfaces/ISearch";
+import { IProfesor } from "@/interfaces/IProfesor";
 
 const ClasesView = () => {
-  // Estados para datos dinámicos
-  const [categories, setCategories] = useState<ICategoria[]>([]); 
+  const [categories, setCategories] = useState<ICategoria[]>([]);
   const [loadingCategories, setLoadingCategories] = useState(true);
   const [errorCategories, setErrorCategories] = useState<string | null>(null);
   const [filteredClasses, setFilteredClasses] = useState<ISearchResult[]>([]);
-  console.log('clases filtradas que enviamos a cardlis:',filteredClasses);
-  
+  const [professorsInFilteredClases, setProfessorsInFilteredClases] = useState<IProfesor[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>("");
-
-  // Estados para filtros
-  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
-  const [selectedProfesor, setSelectedProfesor] = useState<string[]>([]); // Cambié a un array para permitir múltiples profesores seleccionados
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedProfesor, setSelectedProfesor] = useState<string | null>(null);
   const [showFilters, setShowFilters] = useState(false);
+  console.log('categories: ', categories);
+  console.log('errorCategories: ', errorCategories);
+  console.log('filteredClasses: ', filteredClasses);
+  console.log('professorsInFilteredClases: ', professorsInFilteredClases);
+  console.log('error: ', error);
+  console.log('selectedProfesor: ', selectedProfesor);
+  console.log('selectedCategory: ', selectedCategory);
 
-  // Cargar categorías desde la API
   useEffect(() => {
     const fetchCategories = async () => {
       try {
@@ -32,7 +34,6 @@ const ClasesView = () => {
         const data = await getCategories();
         setCategories(data);
       } catch (error) {
-        console.log('error enn categories', error)
         setErrorCategories("Error al cargar las categorías.");
       } finally {
         setLoadingCategories(false);
@@ -42,95 +43,66 @@ const ClasesView = () => {
     fetchCategories();
   }, []);
 
-  // Llamar a la API de clases cuando cambian los filtros o no hay filtros aplicados
   useEffect(() => {
     const fetchClassesData = async () => {
-        setLoading(true);
-        setError(null);
+      setLoading(true);
+      setError(null);
 
-        try {
-            let data: ISearchResult[];
+      try {
+        let data: ISearchResult[];
 
-            if (selectedCategory.length === 0 && selectedProfesor.length === 0) {
-                data = await fetchClases(); // Obtén todas las clases
-            } else {
-                data = await searchClases({
-                    claseNombre: "",
-                    categoriaNombre: selectedCategory.join(","),
-                    perfilProfesorNombre: selectedProfesor.join(","),
-                    descripcion: "",
-                });
-            }
-
-            if (data.length === 0) {
-                setFilteredClasses([]);
-                setError("No se encontraron resultados para tu búsqueda."); // Muestra mensaje amigable
-            } else {
-                setFilteredClasses(data);
-            }
-        } catch (err) {
-            console.error("Error al obtener resultados:", err); // Log de error para depuración
-            setError("Hubo un problema al cargar las clases. Intenta más tarde."); // Mensaje amigable
-        } finally {
-            setLoading(false);
+        if (!selectedCategory && !selectedProfesor) {
+          data = await fetchClases();
+        } else {
+          data = await searchClases({
+            claseNombre: "",
+            categoriaNombre: selectedCategory || "",
+            perfilProfesorNombre: selectedProfesor || "",
+            descripcion: "",
+          });
         }
+
+        if (data.length === 0) {
+          setFilteredClasses([]);
+          setProfessorsInFilteredClases([]);
+          setError("No se encontraron resultados para tu búsqueda.");
+        } else {
+          setFilteredClasses(data);
+
+          const professors = data
+            .map((clase) => clase.perfilProfesor)
+            .filter((profesor) => profesor !== null); // Asegura que no haya nulos
+          setProfessorsInFilteredClases(professors as IProfesor[]); // Asegura que sea de tipo IProfesor
+        }
+      } catch (err) {
+        setError("Hubo un problema al cargar las clases. Intenta más tarde.");
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchClassesData();
-}, [selectedCategory, selectedProfesor]);
+  }, [selectedCategory, selectedProfesor]);
 
-   // Dependencias para recargar la búsqueda cuando cambian los filtros
-
-  // Función para manejar la selección de categorías
-  const handleSelectCategory = (categoryId: string) => {
-    const categoryName = categories.find((cat) => cat.id === categoryId)?.nombre;
-    if (categoryName) {
-      setSelectedCategory((prevState) =>
-        prevState.includes(categoryName)
-          ? prevState.filter((name) => name !== categoryName) // Filtramos el nombre
-          : [...prevState, categoryName] // Agregamos el nombre
-      );
-    }
-  };
-
-  // Función para manejar la selección de profesores
-  const handleSelectProfesor = (profesorId: string | null) => {
-    if (profesorId === null) {
-      setSelectedProfesor([]); // Limpiamos todos los profesores seleccionados
-    } else {
-      const profesorName = profesoresData.find((prof) => prof.id === profesorId)?.nombre;
-      if (profesorName) {
-        setSelectedProfesor((prevState) =>
-          prevState.includes(profesorName)
-            ? prevState.filter((name) => name !== profesorName) // Si el nombre ya está, lo eliminamos
-            : [...prevState, profesorName] // Si no está, lo agregamos
-        );
-      }
-    }
-  };
-
-  // Función para limpiar filtros
   const handleClearFilters = () => {
-    setSelectedCategory([]);
-    setSelectedProfesor([]);
+    setSelectedCategory(null);
+    setSelectedProfesor(null);
   };
 
   return (
     <div>
-      {/* Botón para mostrar/ocultar filtros */}
       <div className="flex justify-start items-center">
         <button
           onClick={() => setShowFilters(!showFilters)}
-          className="text-accent px-4 py-2 rounded"
+          className="text-accent bg-primary px-4 py-2 rounded hover:bg-accent2 transition-colors"
         >
           {showFilters ? "Ocultar Filtros" : "FILTRAR CLASES"}
         </button>
       </div>
 
-      {/* Filtros */}
       {showFilters && (
         <div className="flex flex-col md:flex-row justify-between my-4 gap-4">
-          {/* Filtro por categorías */}
+          {/* Dropdown para Categorías */}
           <div className="flex flex-col">
             <h3 className="text-accent font-bold mb-2">Categorías</h3>
             {loadingCategories ? (
@@ -138,62 +110,73 @@ const ClasesView = () => {
             ) : errorCategories ? (
               <p className="text-red-500">{errorCategories}</p>
             ) : (
-              <div className="flex flex-wrap gap-2">
+              <select
+                value={selectedCategory || ""}
+                onChange={(e) => {
+                  setSelectedCategory(e.target.value || null);
+                  setSelectedProfesor(null);  // Resetea el profesor cuando cambias de categoría
+                }}
+                className="border border-accent bg-secondary2 text-primary rounded px-2 py-1 focus:ring-accent focus:border-accent transition"
+              >
+                <option value="">Selecciona una categoría</option>
                 {categories.map((category) => (
-                  <label key={category.id} className="flex items-center">
-                    <input
-                      type="checkbox"
-                      checked={selectedCategory.includes(category.nombre)} // Cambié category.id por category.nombre
-                      onChange={() => handleSelectCategory(category.id)}
-                      className="mr-2"
-                    />
+                  <option key={category.id} value={category.nombre}>
                     {category.nombre}
-                  </label>
+                  </option>
                 ))}
-              </div>
+              </select>
             )}
           </div>
 
-          {/* Filtro por profesores */}
-          <div className="flex flex-col">
-            <h3 className="text-accent font-bold mb-2">Profesores</h3>
-            <div className="flex flex-wrap gap-2">
-              {profesoresData.map((profesor) => (
-                <label key={profesor.id} className="flex items-center">
-                  <input
-                    type="checkbox"
-                    checked={selectedProfesor.includes(profesor.nombre)} // Usamos array para permitir múltiples selecciones
-                    onChange={() => handleSelectProfesor(profesor.id)} // Al hacer click, añadimos o eliminamos el profesor de la lista
-                    className="mr-2"
-                  />
-                  {profesor.nombre}
-                </label>
-              ))}
-            </div>
-          </div>
+          {/* Dropdown para Profesores */}
+          {selectedCategory && professorsInFilteredClases.length > 0 && (
+  <div className="flex flex-col">
+    <h3 className="text-accent font-bold mb-2">Profesores</h3>
+    <select
+      value={selectedProfesor || ""}
+      onChange={(e) => setSelectedProfesor(e.target.value || null)}
+      className="border border-accent bg-secondary2 text-primary rounded px-2 py-1 focus:ring-accent focus:border-accent transition"
+    >
+      <option value="">Selecciona un profesor</option>
+      {professorsInFilteredClases
+        .filter((profesor) => profesor?.id) // Asegura que profesor tenga un id válido
+        .reduce((unique, profesor) => {
+          // Agregar profesor solo si no existe en el array (eliminando duplicados por id)
+          if (!unique.some((p) => p.id === profesor.id)) {
+            unique.push(profesor);
+          }
+          return unique;
+        }, [] as IProfesor[]) // Inicia el array vacío de IProfesor
+        .map((profesor) => (
+          <option key={profesor.id} value={profesor.nombre}>
+            {profesor.nombre}
+          </option>
+        ))}
+    </select>
+  </div>
+)}
+
+
+
 
           {/* Filtros seleccionados */}
-          <div className="w-full md:w-1/3 bg-gray-100 p-4 rounded">
+          <div className="w-full md:w-1/3 text-primary p-4 rounded shadow-md">
             <h3 className="text-accent font-bold mb-2">Filtros seleccionados</h3>
             <div>
-              {selectedCategory.length > 0 && (
+              {selectedCategory && (
                 <p>
-                  <strong>Categorías:</strong>{" "}
-                  {selectedCategory
-                    .map((name) => categories.find((cat) => cat.nombre === name)?.nombre) // Mapeamos los nombres correctamente
-                    .join(", ")}
+                  <strong>Categoría:</strong> {selectedCategory}
                 </p>
               )}
-              {selectedProfesor.length > 0 && (
+              {selectedProfesor && (
                 <p>
-                  <strong>Profesores:</strong>{" "}
-                  {selectedProfesor.join(", ")}
+                  <strong>Profesor:</strong> {selectedProfesor}
                 </p>
               )}
             </div>
             <button
               onClick={handleClearFilters}
-              className="text-white bg-red-500 py-1 px-3 rounded mt-4"
+              className="text-secondary bg-red-500 py-1 px-3 rounded mt-4 hover:bg-red-600 transition"
             >
               Limpiar filtros
             </button>
@@ -201,27 +184,24 @@ const ClasesView = () => {
         </div>
       )}
 
-      {/* Listado de clases */}
-      <h2 className="text-center font-bold text-xl text-accent my-4">
+      <h2 className="text-center font-bold text-2xl text-accent my-4">
         Clases Disponibles
       </h2>
 
-      {/* Mostramos las clases dependiendo de si hay filtros o no */}
       {loading ? (
-  <p>Cargando clases...</p>
-) : error ? (
-  <p className="text-red-500">{error}</p>
-) : filteredClasses.length === 0 ? (
-  <p className="text-gray-500">No se encontraron resultados para tu búsqueda.</p>
-) : (
-  <ClassCardList classes={filteredClasses} />
-)}
+        <p className="text-primary">Cargando clases...</p>
+      ) : error ? (
+        <p className="text-red-500">{error}</p>
+      ) : filteredClasses.length === 0 ? (
+        <p className="text-secondary2">No se encontraron resultados para tu búsqueda.</p>
+      ) : (
+        <ClassCardList classes={filteredClasses} />
+      )}
     </div>
   );
 };
 
 export default ClasesView;
-
 
 /* import ClassCardList from "@/components/CardList/CardList";
 import Category from "@/components/Categories/Categories";
@@ -243,3 +223,5 @@ const ClasesView= () => {
   
   export default ClasesView
   */
+
+
