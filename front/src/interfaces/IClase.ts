00015666f233f7af49f1aaf518eeb107bf163d27
DiaczukMatias <diaczukm@gmail.com>
2024-11-25 00:05:12 -0300

    cambios Suspendclase

--- front/src/interfaces/IClase.ts
+++ front/src/interfaces/IClase.ts
@@ -1,26 +1,27 @@
 import { ICategoria } from "./ICategory";
-import { IPerfilProfesor} from "./IProfesor";
+import { IPerfilProfesor } from "./IProfesor";
 import { IInscripcion } from "./IInscripcion";
 
 export interface IClase {
-  id: string;                      // Identificador único de la clase (UUID)
-  nombre: string;                  // Nombre de la clase
-  descripcion: string;             // Descripción de la clase
-  fecha: Date | string;      //se modifico      example: 2024-10-10T14:00:00Z       // Fecha y hora de la clase
-  disponibilidad?: number;         // Disponibilidad de espacios (opcional)
-  imagen?: string;                 // URL o ruta de la imagen de la clase (opcional)
+  id: string; // Identificador único de la clase (UUID)
+  nombre: string; // Nombre de la clase
+  descripcion: string; // Descripción de la clase
+  fecha: Date | string; //se modifico      example: 2024-10-10T14:00:00Z       // Fecha y hora de la clase
+  disponibilidad?: number; // Disponibilidad de espacios (opcional)
+  imagen?: string; // URL o ruta de la imagen de la clase (opcional)
   categoriaId: ICategoria["id"]; //nuevo
-  categoria?: ICategoria;         // Categoría a la que pertenece la clase (opcional)
+  categoria?: ICategoria; // Categoría a la que pertenece la clase (opcional)
   inscripciones?: IInscripcion[]; //nuevo
-  perfilProfesor?: IPerfilProfesor;  //cada clase tiene 1 profesor pero cada rpofesor puede tener multiples clases
+  perfilProfesor?: IPerfilProfesor;
+  estado?: "activa" | "suspendida"; //cada clase tiene 1 profesor pero cada rpofesor puede tener multiples clases
 }
 
 export interface ICrearClase {
-  nombre: string;                  // Nombre de la clase
-  descripcion: string;             // Descripción de la clase
-  fecha: string;      //se modifico      example: 2024-10-10T14:00:00Z       // Fecha y hora de la clase
-  disponibilidad: number;         // Disponibilidad de espacios (opcional)
-  imagen: string;                 // URL o ruta de la imagen de la clase (opcional)
-  categoriaId: ICategoria["id"];         // Categoría a la que pertenece la clase (opcional)
+  nombre: string; // Nombre de la clase
+  descripcion: string; // Descripción de la clase
+  fecha: string; //se modifico      example: 2024-10-10T14:00:00Z       // Fecha y hora de la clase
+  disponibilidad: number; // Disponibilidad de espacios (opcional)
+  imagen: string; // URL o ruta de la imagen de la clase (opcional)
+  categoriaId: ICategoria["id"]; // Categoría a la que pertenece la clase (opcional)
   perfilProfesorId: IPerfilProfesor["id"];
 }
