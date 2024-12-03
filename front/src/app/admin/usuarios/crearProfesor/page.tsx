7282da5337cc1c87f1a4c79583030891138e0706
cami-lg96 <clg1196.henry@gmail.com>
2024-12-03 00:16:41 -0300

    crear profesor

---
+++ front/src/app/admin/usuarios/crearProfesor/page.tsx
@@ -0,0 +1,11 @@
+import CrearProfeForm from "@/components/CrearProfesor/CrearProfesor";
+
+export default function crearProfesor() {
+    return (
+      <div className="flex  flex-col justify-center items-center text-center">
+       <CrearProfeForm/>
+      </div>
+    );
+  }
+
+  
