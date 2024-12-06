ac1b06884085602c25e209c4043c12e2889ffea3
cami-lg96 <clg1196.henry@gmail.com>
2024-12-05 22:16:04 -0300

    arreglo titulo planes, sacar titulo datatemporal, arreglo titulo clases admin, not found español

--- front/src/app/admin/clases/page.tsx
+++ front/src/app/admin/clases/page.tsx
@@ -4,7 +4,6 @@ export default function ClasesPage() {
     return (
       <div className="flex  flex-col justify-center items-center text-center">
         <h1 className="text-accent text-2xl font-bold">Gestión de todas las Clases</h1>
-        <p className="mt-4">Contenido relacionado con clases...</p>
         <AllClasesView />
       </div>
     );
